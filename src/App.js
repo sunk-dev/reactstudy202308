bf93b1ab4b0b8aae59e15bccbc56d7f58970fed7
seon <sunkyoung.dev@gmail.com>
2023-08-11 10:12:08 +0900

    컴포넌트 분할하기 ExpenseList

--- src/App.js
+++ src/App.js
@@ -1,45 +1,29 @@
 import React from 'react';
 
 import ExpenseItem from './components/ExpenseItem';
+import ExpenseList from './components/ExpenseList';
 
 const App = () => {
-   //지출항목 객체배열
-  const expenses=[
+  //지출항목 객체배열
+  const expenses = [
     {
-      title:"바나나",
-      price:7000,
-      date:new Date(2022,6-1,21)
+      title: '바나나',
+      price: 7000,
+      date: new Date(2022, 6 - 1, 21),
     },
     {
-      title:"사과",
-      price:5000,
-      date:new Date(2022,7-1,21)
+      title: '사과',
+      price: 5000,
+      date: new Date(2022, 7 - 1, 21),
     },
     {
-      title:"그린애플",
-      price:10000,
-      date:new Date(2022,8-1,21)
-    }
-  ]
+      title: '그린애플',
+      price: 10000,
+      date: new Date(2022, 8 - 1, 21),
+    },
+  ];
   return (
-    <>
-      {/* props->문자외에 다른데이터는 중괄호 안에 넣기 */}
-      <ExpenseItem
-        title={expenses[0].title}
-        price={expenses[0].price}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-         title={expenses[1].title}
-         price={expenses[1].price}
-         date={expenses[1].date}
-      />
-      <ExpenseItem
-         title={expenses[2].title}
-         price={expenses[2].price}
-         date={expenses[2].date}
-      />
-    </>
+      <ExpenseList items={expenses}/>
   );
 };
 
