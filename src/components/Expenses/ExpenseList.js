50b155feb300624cb669d64a237e6f674749d814
seon <sunkyoung.dev@gmail.com>
2023-08-16 11:40:25 +0900

    Chart 기능 기본 js css 구현

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -3,11 +3,13 @@ import ExpenseItem from './ExpenseItem';
 import './ExpenseList.css';
 import Card from '../UI/Card';
 import ExpenseFilter from './ExpenseFilter';
+import ExpensesChart from './ExpenseChart';
 
 const ExpenseList = ({ items }) => {
-
   // 선택된 연도 상태값 관리
-  const [filteredYear, setFilteredYear] = useState(new Date().getFullYear().toString());
+  const [filteredYear, setFilteredYear] = useState(
+    new Date().getFullYear().toString()
+  );
 
   // 자식 컴포넌트 ExpenseFilter에 있는 선택연도를 끌어올리는 콜백함수
   const filterChangeHandler = (selectedYear) => {
@@ -25,29 +27,31 @@ const ExpenseList = ({ items }) => {
   //     />
   //   ));
   // };
-  const filteredItems=items
-  .filter(item => item.date.getFullYear().toString() === filteredYear);
+  const filteredItems = items.filter(
+    (item) => item.date.getFullYear().toString() === filteredYear
+  );
 
   //조건부 랜더링을 위한 변수
-  let expenseContent= <p>아직 등록된 지출이 없습니다</p>;
-  if(filteredItems.length>0){
-    expenseContent=
-    filteredItems.map(({ id, title, price, date }) => (
+  let expenseContent = <p>아직 등록된 지출이 없습니다</p>;
+  if (filteredItems.length > 0) {
+    expenseContent = filteredItems.map(({ id, title, price, date }) => (
       <ExpenseItem
         key={id}
         title={title}
         price={price}
         date={date}
-      />));
+      />
+    ));
   }
-
 
   return (
     <Card className="expenses">
-      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {expenseContent}
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      <ExpensesChart />;{expenseContent}
     </Card>
-
   );
 };
 
