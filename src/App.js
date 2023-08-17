5e055e672290553bee30992d966d05dcaa64243a
seon <sunkyoung.dev@gmail.com>
2023-08-17 11:03:05 +0900

    목표설정 APP 목표 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 import React, { useState } from 'react';
 import CourseInput from './components/CourseGoals/CourseInput';
 import CourseList from './components/CourseGoals/CourseList';
-import './App.css'
+import './App.css';
+
+
+const DUMMY_DATA = [
+  {
+    id: 'g1',
+    text: '리액트 컴포넌트 스타일링 마스터하기'
+  },
+  {
+    id: 'g2',
+    text: 'UI프로그래밍 고수되기'
+  },
+];
+
 const App = () => {
-  //지출항목 객체배열
+
+  const [goals, setGoals] = useState(DUMMY_DATA);
+
+  // courselist 조건부 렌더링
+  let listContent = <p style={{
+    color: 'red',
+    fontSize: '2em',
+    textAlign: 'center'
+  }}>목표를 등록해주세요!!</p>
+  if (goals.length > 0) {
+    listContent = <CourseList items={goals} />;
+  }
+
+  // Input에게 전달할 함수
+  const addGoalHandler = text => {
+    console.log('전달받은 텍스트:', text);
+    const newGoal = {
+      id: Math.random().toString(),
+      text: text
+    }
+
+    // 상태변수(배열) 수정
+    // const updateGoals = [...goals, newGoal];
+    setGoals(prevGoals => [...prevGoals, newGoal]);
+  }
 
   return (
     <div>
       <section id="goal-form">
-        <CourseInput />
+        <CourseInput onAdd={addGoalHandler} />
       </section>
       <section id="goals">
-        <CourseList />
+        {listContent}
       </section>
     </div>
   );
---
+++ src/components/CourseGoals/CourseItem.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import './CourseItem.css'
+
+const CourseItem = ({item}) => {
+  return (
+    <li className='goal-item'>
+        {item.text}
+    </li>
+  )
+}
+
+export default CourseItem
