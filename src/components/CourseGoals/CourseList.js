6dd70dc4638a89166d63b793314eca07c2818ea3
seon <sunkyoung.dev@gmail.com>
2023-08-18 09:24:01 +0900

    styled components

--- src/components/CourseGoals/CourseList.js
+++ src/components/CourseGoals/CourseList.js
@@ -1,15 +1,28 @@
-import React from 'react'
-import './CourseList.css'
+import React from 'react';
+import './CourseList.css';
 import CourseItem from './CourseItem';
+import styled from 'styled-components';
 
-const CourseList = ({items,onDelete}) => {
+const CourseUl = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const CourseList = ({ items, onDelete }) => {
   return (
-    <ul className='goal-list'>
-        {items.map((item)=>{
-            return <CourseItem key={item.id} item={item} onDelete={onDelete}/>
-        })}
-    </ul>
+    <CourseUl>
+      {items.map((item) => {
+        return (
+          <CourseItem
+            key={item.id}
+            item={item}
+            onDelete={onDelete}
+          />
+        );
+      })}
+    </CourseUl>
   );
-}
+};
 
-export default CourseList
+export default CourseList;
